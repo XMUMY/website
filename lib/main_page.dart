5cee502fb7cc330d262c696399c8a22e16a9e7a2
Jason C.H <ctrysbita@outlook.com>
2020-03-13 13:11:03 +0800

    Fix boxfit for cards

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -149,8 +149,19 @@ class _MainPageState extends State<MainPage> {
                     name: 'XMUX Help Center',
                     url: 'https://docs.xmux.xdea.io',
                   ),
+                  MainPageCard(
+                    imageUrl: 'https://s1.ax1x.com/2020/03/13/8nLIGn.jpg',
+                    name: 'Join Development',
+                    url: 'https://docs.xmux.xdea.io/developer/architecture/',
+                    imageFit: BoxFit.fitWidth,
+                  ),
                 ],
-              )
+              ),
+              Divider(height: 60, color: Colors.transparent),
+              Center(
+                child: Text(
+                    'Copyright © 2017-2020 XMUX Project. All rights reserved.'),
+              ),
             ],
           ),
         ],
@@ -164,15 +175,22 @@ class MainPageCard extends StatelessWidget {
   final String name;
   final String url;
 
-  const MainPageCard({Key key, this.imageUrl, this.name, this.url})
+  final BoxFit imageFit;
+
+  const MainPageCard(
+      {Key key,
+      this.imageUrl,
+      this.name,
+      this.url,
+      this.imageFit = BoxFit.contain})
       : super(key: key);
 
   Widget buildCard(BuildContext context) {
     var width = max(350, MediaQuery.of(context).size.width / 4);
-    var height = width * 9 / 16;
+    var height = width * 10 / 16;
 
     return Card(
-      margin: EdgeInsets.all(20),
+      margin: const EdgeInsets.all(20),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Container(
         width: width,
@@ -181,13 +199,20 @@ class MainPageCard extends StatelessWidget {
           children: <Widget>[
             Expanded(
               flex: 3,
-              child: Image.network(imageUrl),
+              child: SizedBox.expand(
+                child: Image.network(
+                  imageUrl,
+                  fit: imageFit,
+                ),
+              ),
             ),
             Expanded(
               flex: 1,
-              child: Text(
-                name,
-                style: Theme.of(context).textTheme.headline5,
+              child: Center(
+                child: Text(
+                  name,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
               ),
             )
           ],
