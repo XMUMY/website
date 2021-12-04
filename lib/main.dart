8bb184d7fb881178f73362585cbab0f160003873
Jason C.H <ctrysbita@outlook.com>
2021-12-05 01:03:11 +0800

    Migrate to NNBD

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:xmux_website/main_page.dart';
 
-void main() => runApp(MainApp());
+void main() => runApp(const App());
 
-class MainApp extends StatelessWidget {
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'XMUX Official Website',
-      theme: ThemeData.dark(),
-      home: MainPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        brightness: Brightness.dark,
+      ),
+      home: const MainPage(),
     );
   }
 }
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
   @override
   _MainPageState createState() => _MainPageState();
 }
@@ -17,9 +19,10 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     foregroundController.addListener(() {
-      if (mounted)
+      if (mounted) {
         setState(() =>
             sigma = min(max((foregroundController.offset - 150) / 15, 0), 6.0));
+      }
     });
     super.initState();
   }
@@ -38,36 +41,41 @@ class _MainPageState extends State<MainPage> {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
           child: Card(
+            elevation: 10,
+            clipBehavior: Clip.hardEdge,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(30),
             ),
-            elevation: 10,
             child: Image.network(
               'https://s2.ax1x.com/2020/03/03/34hk6I.jpg',
               scale: 1.6,
             ),
           ),
         ),
-        TyperAnimatedTextKit(
-          text: [
-            'A powerful app for XMUMers',
-            '为 XMUMers 设计的强大应用',
+        AnimatedTextKit(
+          animatedTexts: [
+            TyperAnimatedText(
+              'A powerful app for XMUMers',
+              textStyle: const TextStyle(fontSize: 25.0, fontFamily: 'Agne'),
+            ),
+            TyperAnimatedText(
+              '为 XMUMers 设计的强大应用',
+              textStyle: const TextStyle(fontSize: 25.0, fontFamily: 'Agne'),
+            ),
           ],
-          speed: const Duration(milliseconds: 50),
           pause: const Duration(milliseconds: 3000),
-          textStyle: TextStyle(fontSize: 25.0, fontFamily: 'Agne'),
-          textAlign: TextAlign.left,
-          alignment: AlignmentDirectional.topStart,
         ),
       ],
     );
 
     var downloads = Column(
       children: <Widget>[
-        FlatButton(
-          padding: const EdgeInsets.all(15),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.all(15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
           onPressed: () => launch(
               'https://play.google.com/store/apps/details?id=org.ctbeta.xmux.xmux&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'),
@@ -76,36 +84,25 @@ class _MainPageState extends State<MainPage> {
             scale: 2.0,
           ),
         ),
-        FlatButton(
-          padding: const EdgeInsets.all(15),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.all(15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
           onPressed: () =>
               launch('https://itunes.apple.com/my/app/xmux/id1366324008'),
           child: Image.network(
             'https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg',
-            scale: 0.45,
-          ),
-        ),
-        Padding(padding: const EdgeInsets.all(10)),
-        FlatButton(
-          padding: const EdgeInsets.all(15),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          onPressed: () => launch(
-              'https://www.microsoft.com/zh-cn/p/xmum/9n3mtxt08tv2?cid=storebadge&ocid=badge&rtc=1'),
-          child: Image.network(
-            'https://assets.windowsphone.com/85864462-9c82-451e-9355-a3d5f874397a/English_get-it-from-MS_InvariantCulture_Default.png',
-            scale: 3.3,
+            scale: 0.43,
           ),
         ),
       ],
     );
 
     return Scaffold(
-      backgroundColor: Color(0xFF10052F),
+      backgroundColor: const Color(0xFF10052F),
       body: Stack(
         children: <Widget>[
           SizedBox.expand(
@@ -125,7 +122,7 @@ class _MainPageState extends State<MainPage> {
           ListView(
             padding: const EdgeInsets.all(10),
             controller: foregroundController,
-            physics: BouncingScrollPhysics(),
+            physics: const BouncingScrollPhysics(),
             children: <Widget>[
               Divider(
                 height: MediaQuery.of(context).size.height / 4.0,
@@ -139,10 +136,10 @@ class _MainPageState extends State<MainPage> {
                   downloads,
                 ],
               ),
-              Divider(height: 60, color: Colors.transparent),
+              const Divider(height: 60, color: Colors.transparent),
               Wrap(
                 alignment: WrapAlignment.spaceEvenly,
-                children: <Widget>[
+                children: const [
                   MainPageCard(
                     imageUrl: 'https://s1.ax1x.com/2020/03/31/Gu47ZV.png',
                     name: 'XMUX Help Center',
@@ -156,10 +153,12 @@ class _MainPageState extends State<MainPage> {
                   ),
                 ],
               ),
-              Divider(height: 60, color: Colors.transparent),
-              Center(
+              const Divider(height: 60, color: Colors.transparent),
+              const Center(
                 child: Text(
-                    'Copyright © 2017-2020 XMUX Project. All rights reserved.'),
+                  'Copyright © 2017-2021 XMUX Project. All rights reserved.',
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
           ),
@@ -173,25 +172,25 @@ class MainPageCard extends StatelessWidget {
   final String imageUrl;
   final String name;
   final String url;
-
   final BoxFit imageFit;
 
-  const MainPageCard(
-      {Key key,
-      this.imageUrl,
-      this.name,
-      this.url,
-      this.imageFit = BoxFit.contain})
-      : super(key: key);
+  const MainPageCard({
+    Key? key,
+    required this.imageUrl,
+    required this.name,
+    required this.url,
+    this.imageFit = BoxFit.contain,
+  }) : super(key: key);
 
   Widget buildCard(BuildContext context) {
-    var width = max(350, MediaQuery.of(context).size.width / 4);
+    var width = max(350.0, MediaQuery.of(context).size.width / 4);
     var height = width * 10 / 16;
 
     return Card(
       margin: const EdgeInsets.all(20),
+      clipBehavior: Clip.hardEdge,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Container(
+      child: SizedBox(
         width: width,
         height: height,
         child: Column(
@@ -224,8 +223,12 @@ class MainPageCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Theme(
       data: ThemeData.light(),
-      child: OutlineButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
         onPressed: () => launch(url),
         child: Builder(builder: buildCard),
       ),
